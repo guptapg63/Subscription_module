6988f35b1cd3f2177ea36e0d73dfa741db3d6b5c
guptapg63 <139619152+guptapg63@users.noreply.github.com>
2023-11-16 21:02:21 +0530

    Subscription_Module

---
+++ subscription module/index.js
@@ -0,0 +1,18 @@
+//$("p").css("color","green");
+
+
+//$(document).ready(function(){
+  //  $("p").click(function(){
+    //  $(this).hide();
+    //});
+  //});
+
+  //$("p").css("font-size","2rem");
+
+ // $(document).ready(function(){
+   // $("#up").click(function(){
+     // $("#up2").slideUp("slow");
+    //});
+  //});
+
+  //some functionalities which work on clicking but not in use for all together//
